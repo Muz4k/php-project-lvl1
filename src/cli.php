93009fc96bb19fe328e6757dce471e78284e9889
Muz4k <muz4k.oo@gmail.com>
2019-10-17 22:48:15 +0300

    Подключение сторонней библиотеки, создание Makefile и файла с исходными данными. Добавление кода интерактивной программы.

---
+++ src/cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function run()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
